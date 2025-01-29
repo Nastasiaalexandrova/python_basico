24dddda0c926ba079d5f7b6910685e0767bb5464
Anastasiia Mishakova <mishakovaanastasiya@gmail.com>
2025-01-29 20:10:48 +0100

    Un mensaje 29/01 Ultimo

--- 04_strings.py
+++ 04_strings.py
@@ -6,3 +6,10 @@ string_1 = 'Hola'
 string_2 = "Hola"
 string_3 = """Hola"""
 print(string_1, string_2, string_3)
+
+name = "Anastasiia"
+surname = "Mishakova"
+age = 35
+phrase = "Hola. Me llamo" + " " + name + " " + surname + "." + " Yo tengo " + str(age) + " años"
+print(phrase)
+print
